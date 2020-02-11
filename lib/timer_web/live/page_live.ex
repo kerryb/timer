4f6de6b998033a972a472ec75c04163e0c87396c
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-11 14:33:12 +0000

    Add handler for start button

--- lib/timer_web/live/page_live.ex
+++ lib/timer_web/live/page_live.ex
@@ -8,4 +8,9 @@ defmodule TimerWeb.PageLive do
   def mount(_params, _session, socket) do
     {:ok, socket |> assign(:seconds_remaining, 60)}
   end
+
+  def handle_event("start", _data, socket) do
+    IO.puts("Start button pressed")
+    {:noreply, socket}
+  end
 end
