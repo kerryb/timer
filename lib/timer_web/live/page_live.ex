ef2001e55f77ed28fe3a157ee5ed7db813a22272
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-11 14:36:53 +0000

    Toggle start/stop button

--- lib/timer_web/live/page_live.ex
+++ lib/timer_web/live/page_live.ex
@@ -6,11 +6,16 @@ defmodule TimerWeb.PageLive do
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:seconds_remaining, 60)}
+    {:ok, socket |> assign(seconds_remaining: 60, running: false)}
   end
 
   def handle_event("start", _data, socket) do
     IO.puts("Start button pressed")
-    {:noreply, socket}
+    {:noreply, socket |> assign(:running, true)}
+  end
+
+  def handle_event("stop", _data, socket) do
+    IO.puts("Stop button pressed")
+    {:noreply, socket |> assign(:running, false)}
   end
 end
