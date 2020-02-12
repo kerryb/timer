d947e74fe7ddc46873cd123123189dcb4605259a
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-12 11:54:05 +0000

    Add klaxon sound to page

--- lib/timer_web/endpoint.ex
+++ lib/timer_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule TimerWeb.Endpoint do
     at: "/",
     from: :timer,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt sounds)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
