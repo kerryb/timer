5ccbcce95d5b42c9307daed410e3ed8e44dd8376
Kerry Buckley <kerryjbuckley@gmail.com>
2020-03-08 18:47:42 +0000

    Toggle start/stop buttons

--- lib/timer_web/live/page_live.ex
+++ lib/timer_web/live/page_live.ex
@@ -7,6 +7,19 @@ defmodule TimerWeb.PageLive do
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(seconds: 3)}
+    {:ok, socket |> assign(seconds: 3, running: false)}
+  end
+
+  def handle_event("start", _data, socket) do
+    {:noreply, socket |> assign(running: true)}
+  end
+
+  def handle_event("stop", _data, socket) do
+    {:noreply, socket |> assign(running: false)}
+  end
+
+  def handle_event(event, data, socket) do
+    Logger.debug("Received event #{inspect(event)}, with data #{inspect(data)}")
+    {:noreply, socket}
   end
 end
